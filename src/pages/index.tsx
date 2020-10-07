023637064d8ad2d4873ff7d6654835862235a388
Gleyson Carvalho <gleyson_10@hotmail.com>
2020-10-06 22:28:59 -0300

    base project

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => (
+  <div>
+    <Head>
+      <title>HomePage</title>
+    </Head>
+
+    <main>
+      <h1>Hello Word</h1>
+    </main>
+  </div>
+)
+
+export default Home
